489e60f70f37e2691bded6e3308c25cf2a53d7a2
Lenz Weber <mail@lenzw.de>
2018-02-07 20:13:28 +0100

    fix for react-native .47+ (see https://github.com/react-native-community/react-native-blur/pull/226)

--- android/src/main/java/com/reactlibrary/RNTesseractOcrPackage.java
+++ android/src/main/java/com/reactlibrary/RNTesseractOcrPackage.java
@@ -16,7 +16,6 @@ public class RNTesseractOcrPackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNTesseractOcrModule(reactContext));
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
